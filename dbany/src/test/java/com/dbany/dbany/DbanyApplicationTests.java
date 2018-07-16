704975bf01d744126802193084a2910ce03ad41c
skyshop300 <skyshop5210@gmail.com>
2018-07-16 13:33:44 +0900

    spring setting

---
+++ dbany/src/test/java/com/dbany/dbany/DbanyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dbany.dbany;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DbanyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
